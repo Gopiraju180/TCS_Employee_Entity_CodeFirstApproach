2a51e80c6ea6ef24d6dd0af6c9b9f8134b35f7ec
hp <hp@DESKTOP-LNEME22>
2024-12-05 15:40:10 +0530

    UserStory_586: OrdersController And All relatedMethods Implemented

---
+++ TCS_Employee_Entity_CodeFirstApproach/Interfaces/IDepartementRepository.cs
@@ -0,0 +1,13 @@
+﻿using TCS_Employee_Entity_CodeFirstApproach.Entites;
+
+namespace TCS_Employee_Entity_CodeFirstApproach
+{
+    public interface IDepartementRepository
+    {
+        Task<List<Departement>> GetDepartMentDetails();
+        Task<Departement> GetDepartmentDetailsById(int deptid);
+        Task<int> AddDeparment(Departement deptdetail);
+        Task<bool> DeleteDepartmentById(int deptid);
+        Task<bool> UpdateDepartment(Departement deptdetail);
+    }
+}
---
+++ TCS_Employee_Entity_CodeFirstApproach/Repositories/DepartementRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCS_Employee_Entity_CodeFirstApproach.Entites;
+
+namespace TCS_Employee_Entity_CodeFirstApproach.Repositories
+{
+    public class DepartementRepository : IDepartementRepository
+    {
+        private readonly EmployeeContext _employeeContext;
+        public DepartementRepository(EmployeeContext employeeContext)
+        {
+                _employeeContext = employeeContext;
+        }
+        public async Task<int> AddDeparment(Departement deptdetail)
+        {
+            await _employeeContext.departements.AddAsync(deptdetail);//add the record by using addasync
+            _employeeContext.SaveChanges();//it will commit/save the data perminently in table
+            return 1;
+        }
+
+        public async Task<bool> DeleteDepartmentById(int deptid)
+        {
+            Departement rm = _employeeContext.departements.SingleOrDefault(e => e.deptid == deptid);
+            if (rm != null)
+            {//Here Remove() method is used for removing the data from database.
+
+                _employeeContext.departements.Remove(rm);
+                _employeeContext.SaveChanges();
+                return true;
+            }
+            else return false;
+        }
+
+        public async Task<List<Departement>> GetDepartMentDetails()
+        {
+            var result = _employeeContext.departements.ToList();
+            if (result.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public async Task<Departement> GetDepartmentDetailsById(int deptid)
+        {
+            var rm = await _employeeContext.departements.Where(e => e.deptid == deptid).FirstOrDefaultAsync();
+
+            if (rm == null)
+                return null;
+            else
+                return rm;
+        }
+
+        public async Task<bool> UpdateDepartment(Departement deptdetail)
+        {
+            _employeeContext.Update(deptdetail);
+            await _employeeContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
